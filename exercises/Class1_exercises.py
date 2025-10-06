5b71bd0f53005f6200a99e81d9d2f98d8c143419
Maria Alexandra Oliveira <43906345+MAlexandraOliveira@users.noreply.github.com>
2025-10-06 14:31:01 +0100

    Add files via upload
    Added exercises for class 1

---
+++ exercises/Class1_exercises.py
@@ -0,0 +1,146 @@
+'''
+EXERCISES FOR CLASS 1
+'''
+
+'''
+Exercise 1
+Create a class called "Book" with attributes "title" and "author".
+Instantiate one Book object and print its attributes.
+'''
+# ANSWER
+
+
+'''
+Exercise 2
+Add a method "description" to the Book class that prints:
+"Title: <title>, Author: <author>".
+Create a book and call this method.
+'''
+# ANSWER
+
+
+'''
+Exercise 3
+Create a class "Rectangle" with attributes "width" and "height".
+Add a method "area" that returns the area of the rectangle.
+Test it with width=5 and height=3.
+'''
+# ANSWER
+
+
+'''
+Exercise 4
+Extend the Rectangle class by adding a method "perimeter" 
+that calculates the perimeter of the rectangle.
+'''
+# ANSWER
+
+
+'''
+Exercise 5
+Create a class "Student" with attributes "name" and "grade".
+Add a constructor (__init__) to initialize these attributes.
+Print the student's name and grade.
+'''
+# ANSWER
+
+
+'''
+Exercise 6
+Modify the Student class to include a method "is_passing"
+that returns True if the grade is >= 10, otherwise False.
+'''
+# ANSWER
+
+
+'''
+Exercise 7
+Create a class "BankAccount" with attribute "balance" initialized to 0.
+Add methods "deposit" and "withdraw" to modify the balance,
+and a method "get_balance" to return the balance.
+Test the class.
+'''
+# ANSWER
+
+
+'''
+Exercise 8
+Create a class "Library" with a class variable "library_name" set to "Central Library".
+Each book added should be stored in an instance variable "books" (a list).
+Add methods to add a book and list all books.
+'''
+# ANSWER
+
+
+'''
+Exercise 9
+Create a class "Temperature".
+Add a class method "from_fahrenheit" that takes a Fahrenheit value
+and returns a Temperature object in Celsius.
+Also add a method "to_fahrenheit" to convert back to Fahrenheit.
+'''
+# ANSWER
+
+
+'''
+Exercise 10
+Create a class "MathTools" with a static method "is_even" 
+that returns True if a number is even, otherwise False.
+Demonstrate its use on numbers 7 and 10.
+'''
+# ANSWER
+
+
+'''
+Exercise 11
+Use type() and dir() to check the type and available methods 
+for a variable my_list = [1, 2, 3].
+Print the results and then call append() to add a new element.
+'''
+# ANSWER
+
+
+'''
+Exercise 12
+Create a class "Employee" with attributes "name" and "salary".
+Add a method "raise_salary" that increases the salary by a given percentage.
+Create one employee, raise their salary by 15%, and print the result.
+'''
+# ANSWER
+
+
+'''
+Exercise 13
+Create a class "ShoppingCart" with an attribute "items" (a list).
+Add methods "add_item" (adds an item), "remove_item" (removes an item if it exists),
+and "checkout" (prints the total number of items).
+Test with at least 3 items.
+'''
+# ANSWER
+
+
+'''
+Exercise 14
+Complex Task:
+Create a class "Course" with attributes "title" and "students" (a list).
+Add methods to enroll a student, list students, and count students.
+Also add a class variable "school" = "Data Academy".
+Test with multiple students.
+'''
+# ANSWER
+
+
+'''
+Exercise 15
+Complex Task:
+Create a class "Company" with attributes "name" and "employees" (a list).
+- Add a method to hire an employee (an object of class Employee).
+- Add a method to list employees with their salaries.
+- Add a class method "from_list" that creates a Company 
+  directly from a list of (name, salary) tuples.
+- Add a static method "average_salary" that takes a list of employees
+  and returns the average salary.
+
+Test all methods.
+'''
+# ANSWER
